ab52fd7712ef2948ba18ddbecec58d9af1a7f59f
Fernando Eickhoff <fernando.eickhoff@sensedia.com>
2021-02-15 13:55:30 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Kafka.Public;
+using Kafka.Public.Loggers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace KafkaProducerDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((context, collection) =>
+                {
+                    collection.AddHostedService<KafkaProducerHostedService>();
+                });
+    }
+
+    public class KafkaProducerHostedService : IHostedService
+    {
+        private readonly ILogger<KafkaProducerHostedService> _logger;
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducerHostedService(ILogger<KafkaProducerHostedService> logger)
+        {
+            _logger = logger;
+            var config = new ProducerConfig()
+            {
+                BootstrapServers = "localhost:9092"
+            };
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+        
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            for (var i = 0; i < 100; ++i)
+            {
+                var value = $"My Demo Message Number {i}";
+                _logger.LogInformation(value);
+                await _producer.ProduceAsync("itr-demo", new Message<Null, string>()
+                {
+                    Value = value
+                }, cancellationToken);
+            }
+
+            _producer.Flush(TimeSpan.FromSeconds(10));
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _producer?.Dispose();
+            return Task.CompletedTask;
+        }
+    }
+}
